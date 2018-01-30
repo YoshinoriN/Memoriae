5a5b2b849bd8c5bda8f1cae7ac7f8ed7fa63791e
YoshinoriN <nayo-msoft@outlook.jp>
2018-01-30 22:29:51 +0900

    Create ConfigProvider trait and DBConfig object

---
+++ src/scala/app/utils/config/DBConfig.scala
@@ -0,0 +1,9 @@
+package app.utils.config
+
+object DBConfig extends ConfigProvider {
+
+  val url = configuration.getString("db.ctx.dataSource.url")
+  val user = configuration.getString("db.ctx.dataSource.user")
+  val password = configuration.getString("db.ctx.dataSource.password")
+
+}
