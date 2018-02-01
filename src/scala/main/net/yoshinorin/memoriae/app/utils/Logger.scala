b86e1378c69a5de9b2351fd2ac26095c52a99dea
YoshinoriN <nayo-msoft@outlook.jp>
2018-02-01 22:46:40 +0900

    Create Logger trait

---
+++ src/scala/main/net/yoshinorin/memoriae/app/utils/Logger.scala
@@ -0,0 +1,13 @@
+package net.yoshinorin.memoriae.utils
+
+import org.slf4j.{ LoggerFactory, MarkerFactory }
+
+/**
+ * Implement this to get a named logger in scope.
+ */
+trait Logger {
+
+  val logger = LoggerFactory.getLogger(this.getClass)
+  val securityMaker = MarkerFactory.getMarker("SECURITY")
+
+}
