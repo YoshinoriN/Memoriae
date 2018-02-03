bcf58d1933df810d120d3e7d486ea55f845f2658
YoshinoriN <nayo-msoft@outlook.jp>
2018-02-04 01:30:40 +0900

    Update Users case class

--- src/app/models/Users.scala
+++ src/app/models/Users.scala
@@ -1,13 +1,15 @@
 package memoriae.models
 
+import java.time.LocalDateTime
+
 case class Users(
   id: Long,
   userName: String,
   password: String,
-  isAdmin: Option[Boolean] = Some(false),
-  forcePasswordReset: Option[Boolean] = Some(false),
-  lastSigninAt: Option[java.sql.Timestamp] = None,
-  createdAt: java.sql.Timestamp = new java.sql.Timestamp(System.currentTimeMillis()),
-  updatedAt: Option[java.sql.Timestamp] = None,
-  deletedAt: Option[java.sql.Timestamp] = None
+  isAdmin: Boolean,
+  forcePasswordReset: Boolean,
+  lastSigninAt: Option[LocalDateTime],
+  createdAt: LocalDateTime,
+  updatedAt: LocalDateTime,
+  deletedAt: Option[LocalDateTime]
 )
