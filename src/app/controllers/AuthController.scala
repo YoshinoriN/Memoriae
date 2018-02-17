23764f22c00d2802826e942c8132a82df43dbad1
YoshinoriN <nayo-msoft@outlook.jp>
2018-02-18 01:53:12 +0900

    Create sign in page

--- src/app/controllers/AuthController.scala
+++ src/app/controllers/AuthController.scala
@@ -6,8 +6,14 @@ import play.api.i18n.{ I18nSupport, Messages }
 import play.api.mvc.{ AbstractController, AnyContent, ControllerComponents, Request }
 import play.api.data.Form
 import play.api.data.Forms._
+import com.mohiva.play.silhouette.api._
+import com.mohiva.play.silhouette.api.repositories.AuthInfoRepository
+import com.mohiva.play.silhouette.api.util.{ PasswordHasherRegistry, PasswordInfo }
+import com.mohiva.play.silhouette.impl.providers._
+import com.mohiva.play.silhouette.api.actions.SecuredRequest
 import controllers.AssetsFinder
 import memoriae.utils.Logger
+import memoriae.utils.auth.JWTEnv
 
 import AuthController._
 
@@ -16,13 +22,14 @@ object AuthController {
 
   val signInForm = Form(
     mapping(
-      "uid" -> nonEmptyText,
+      "id" -> nonEmptyText,
       "password" -> nonEmptyText,
     )(SignInFormData.apply)(SignInFormData.unapply)
   )
 }
 
 class AuthController @Inject() (
+  silhouette: Silhouette[JWTEnv],
   components: ControllerComponents
 )(
   implicit
@@ -30,7 +37,9 @@ class AuthController @Inject() (
   ex: ExecutionContext
 ) extends AbstractController(components) with I18nSupport with Logger {
 
-  def view = TODO
+  def view = silhouette.UnsecuredAction.async { implicit request: Request[AnyContent] =>
+    Future.successful(Ok(views.html.signIn(signInForm)))
+  }
 
   def signIn = TODO
 
