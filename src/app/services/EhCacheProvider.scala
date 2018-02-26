a9bd89b0d2daf2731ddd72fe56a0a80b8f9e5a54
YoshinoriN <nayo-msoft@outlook.jp>
2018-02-26 23:46:23 +0900

    Implement EhCacheProvider trait

---
+++ src/app/services/EhCacheProvider.scala
@@ -0,0 +1,29 @@
+package memoriae.services
+
+import scala.concurrent.duration.{ Duration, _ }
+import scala.concurrent.{ Await, Future }
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.reflect.ClassTag
+import play.api.cache.ehcache.EhCacheApi
+import net.sf.ehcache.CacheManager
+
+trait EhCacheProvider {
+
+  val name: String
+  private val cacheManager = CacheManager.create
+  cacheManager.addCache(name)
+
+  private val cache = new EhCacheApi(cacheManager.getEhcache(name))
+
+  def get[T](key: String, dur: Duration = Duration.Inf)(implicit cTag: ClassTag[T]): Option[T] = {
+    Await.result(cache.get[T](key), dur)
+  }
+
+  def set(key: String, value: Any, dur: Duration = Duration.Inf): Unit = {
+    cache.set(key, value, dur)
+  }
+
+  def remove(key: String): Unit = {
+    cache.remove(key)
+  }
+}
