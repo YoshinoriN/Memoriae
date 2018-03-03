c2c6ac31ad9c3b7644ca9e8896e5893ec1e4217a
YoshinoriN <nayo-msoft@outlook.jp>
2018-03-03 22:24:35 +0900

    Implement UserDAO class each methods

--- src/app/models/daos/UserIdentifyDAO.scala
+++ src/app/models/daos/UserIdentifyDAO.scala
@@ -1,19 +1,29 @@
 package memoriae.models.daos
 
 import scala.concurrent.Future
+import scala.concurrent.duration.{ Duration, _ }
 import com.mohiva.play.silhouette.api.{ Identity, LoginInfo }
 import memoriae.models.UserIdentify
+import memoriae.services.cache.UserAuthenticationCache
 import memoriae.utils.Logger
 
 class UserDAO extends UserDAOTrait with Logger {
 
   def find(loginInfo: LoginInfo) = {
-    Future.successful(None)
+    UserAuthenticationCache.cache.get[UserIdentify](loginInfo.providerKey) match {
+      case Some(user) => Future.successful(Option(user))
+      case None => {
+        logger.info(securityMaker, s"Can not find authenticated information : ${loginInfo.providerKey}")
+        Future.successful(None)
+      }
+    }
   }
 
-  def find(userName: String) = Future.successful(None)
+  def find(userName: String) = Future.successful(UserAuthenticationCache.cache.get[UserIdentify](userName))
 
   def save(user: UserIdentify) = {
+    //TODO: Set duration
+    UserAuthenticationCache.cache.set(user.userName, user, Duration.Inf)
     Future.successful(user)
   }
 
